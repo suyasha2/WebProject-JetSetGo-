35fc170e6fe60bcef2875c0b4ef7700f59ce3e5f
suyasha2 <suyashaneupane64@gmail.com>
2026-02-28 11:42:31 +0545

    Notification Model

---
+++ backend/models/Notification.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const Notification = sequelize.define("Notification", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: { type: DataTypes.STRING, allowNull: false }, 
+  desc: { type: DataTypes.TEXT, allowNull: false },    
+  type: { type: DataTypes.STRING, defaultValue: "info" } 
+}, {
+  timestamps: true 
+});
+
+module.exports = Notification;
