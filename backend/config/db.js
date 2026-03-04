36ddf8a07b1600fb8bf9ce8196c89cc441c6b42e
suyasha2 <suyashaneupane64@gmail.com>
2026-03-04 13:52:27 +0545

    Config

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || 'your_db_password', 
   {
     host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5433, // <--- PGAdmin ko port check garnus (5432 or 5433)
+    port: process.env.DB_PORT || 5433, 
     dialect: 'postgres',
     logging: false,
   }
